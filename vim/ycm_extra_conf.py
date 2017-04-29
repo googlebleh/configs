d51e9b1ae822416d2cfeaf2caf9309133c19ab09
Colin Wee <2gbleh@gmail.com>
2017-04-29 10:43:44 -0400

    add ycm default config; add molokai theme; update line limit script

---
+++ vim/ycm_extra_conf.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import os
+
+
+def FlagsForFile(fpath, **kwargs):
+    name, ext = os.path.splitext(fpath)
+    
+    if ext == '.c':
+        flags = ['-x', 'c', '-std=c11', '-Wall', '-Wextra', '-Werror']
+        return {'flags': flags}
+    
+    elif ext in {'.cpp', '.h'}:
+        flags = ['-x', 'c++', '-std=c++11', '-Wall', '-Wextra', '-Werror']
+        return {'flags': flags}
