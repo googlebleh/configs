c3392037cb4e377b4518b37c647ce21b5c93af48
Colin Wee <2gbleh@gmail.com>
2018-07-19 18:13:04 -0400

    add minimal install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+configs_root=$1
+
+ln -s "$configs_root/Xresources" ~/.Xresources
+ln -s "$configs_root/gitconfig" ~/.gitconfig
+ln -s "$configs_root/tmux.conf" ~/.tmux.conf
+
+mkdir -p ~/.vim/autoload
+wget \
+  --output-document=~/.vim/autoload/plug.vim \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln -s "$configs_root/vim/after" "$HOME/.vim"
+ln -s "$configs_root/vim/vimrc" "$HOME/.vimrc"
