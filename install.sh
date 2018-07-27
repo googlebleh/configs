728e6503a5a7d497141e89b1c0fe594e5a750d91
Colin Wee <2gbleh@gmail.com>
2018-07-27 16:32:43 -0400

    install: bash style fixes

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@ set -ex
 
 configs_root=$1
 
-ln -s "$configs_root/Xresources" ~/.Xresources
-ln -s "$configs_root/gitconfig" ~/.gitconfig
-ln -s "$configs_root/tmux.conf" ~/.tmux.conf
+ln -s "${configs_root}/Xresources" ~/.Xresources
+ln -s "${configs_root}/gitconfig" ~/.gitconfig
+ln -s "${configs_root}/tmux.conf" ~/.tmux.conf
 
 mkdir -p ~/.vim/autoload
 wget \
-  --output-document="$HOME/.vim/autoload/plug.vim" \
+  --output-document="${HOME}/.vim/autoload/plug.vim" \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -s "$configs_root/vim/after" "$HOME/.vim"
-ln -s "$configs_root/vim/vimrc" "$HOME/.vimrc"
+ln -s "${configs_root}/vim/after" "${HOME}/.vim"
+ln -s "${configs_root}/vim/vimrc" "${HOME}/.vimrc"
