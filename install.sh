84987d8a420627717fbf8b50e699f0e716647b82
Colin Wee <2gbleh@gmail.com>
2021-06-24 10:25:47 -0700

    vim: neovim compatibility
    A Linux share I use regularly has neovim installed as the default vim.

--- install.sh
+++ install.sh
@@ -4,19 +4,27 @@ set -ex
 
 configs_root=$1
 
-if [ ! -d "${configs_root}" ]; then
-  echo "Invalid directory \"${configs_root}\""
-  echo "usage: ./install.sh <config-root-dir>"
-  exit 1
+if [ ! -d "$configs_root" ]; then
+    echo "Invalid directory \"$configs_root\""
+    echo "usage: ./install.sh <config-root-dir>"
+    exit 1
 fi
 
-ln -s "${configs_root}/gitconfig" ~/.gitconfig
-ln -s "${configs_root}/inputrc" ~/.inputrc
-ln -s "${configs_root}/tmux.conf" ~/.tmux.conf
+ln -s "$configs_root/gitconfig" ~/.gitconfig
+ln -s "$configs_root/inputrc" ~/.inputrc
+ln -s "$configs_root/tmux.conf" ~/.tmux.conf
 
-mkdir -p ~/.vim/autoload
-wget \
-  --output-document="${HOME}/.vim/autoload/plug.vim" \
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -s "${configs_root}/vim/after" ~/.vim
-ln -s "${configs_root}/vim/vimrc" ~/.vimrc
+if vim --version | grep -q "^NVIM"; then
+    mkdir -p ~/.vim
+    curl -fL \
+        -o "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim \
+        --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+else
+    mkdir -p ~/.vim/autoload
+    wget \
+        --output-document="${HOME}/.vim/autoload/plug.vim" \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+ln -s "$configs_root/vim/after" ~/.vim
+ln -s "$configs_root/vim/vimrc" ~/.vimrc
